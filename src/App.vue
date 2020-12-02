b3a9f30f318f33e9f64e4e079aaa0d96ae029880
Ankit Srivastava <ankitsrivas14@gmail.com>
2020-12-02 17:50:25 +0530

    Todo list boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1>Hello Ankit Srivastava</h1>
+  <div>
+    <h1>Hello Ankit Srivastava</h1>
+    <TodoList />
+  </div>
 </template>
 
 <script>
+import TodoList from "./components/TodoList"
+
 export default {
-  
+  name: "App",
+  components: {
+    TodoList
+  }
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>Hello from Todo list template</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            todos:[
+                {todoString: "Complete Vue Course", completed:false},
+                {todoString: "Order food", completed:true},
+                {todoString: "Send documents", completed:false},
+                {todoString: "Study CSS animations", completed:true},
+                {todoString: "Recharge mobile", completed:false}
+            ]
+        }
+    },
+    methods:{
+        addTodo(newTodo){
+            this.todos.push(
+                {todoString:newTodo,completed:false}
+            )
+        },
+        toggleTodo(todo){
+            todo.completed = !todo.completed
+        },
+        editTodo(todo,newTodoString){
+            todo.todoString = newTodoString
+        },
+        deleteTodo(deleteTodo){
+            this.todos = this.todos.filter(todo => todo.todoString !== deleteTodo.todoString)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
